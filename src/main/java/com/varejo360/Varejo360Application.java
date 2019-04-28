4bacf608805fbec7ec5853f6056b48a9c7950849
Porto <portinhoc10@gmail.com>
2019-04-28 12:50:22 -0300

    First commit

---
+++ src/main/java/com/varejo360/Varejo360Application.java
@@ -0,0 +1,13 @@
+package com.varejo360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Varejo360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Varejo360Application.class, args);
+	}
+
+}
