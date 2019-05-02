47c10c4a1e08dc4fb5780dc6ee0384893575b540
Porto <portinhoc10@gmail.com>
2019-05-02 13:54:34 -0300

    Fix url csv

--- src/main/java/com/varejo360/constant/ApiMapping.java
+++ src/main/java/com/varejo360/constant/ApiMapping.java
@@ -5,7 +5,7 @@ public class ApiMapping {
 	public ApiMapping() {}
 
 	public static final String API = "/api/";
-	public static final String FILE = "/Users/joaopedroporto/Downloads/dados.csv";
+	public static final String FILE = "././././././files/dados.csv";
 	public static final String CONTEXT = "";
 	public static final String PRODUCT = API + "product";
 	
